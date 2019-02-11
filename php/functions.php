01143e4333d03e7fba22064fe2cec9db2a8240c0
Samme Robbroeckx <Samme.Robbroeckx@securitas.be>
2019-02-11 10:29:34 +0100

    added php function link database

--- php/functions.php
+++ php/functions.php
@@ -2,6 +2,25 @@
 
 //PHP Functions
 
+//LinkDB
+function LinkDB()
+{
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password);
+
+	// Check connection
+	if (!$conn) 
+	{
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	
+	return $conn;
+}
+
 //Login
 function Login($pass, $mail)
 {
